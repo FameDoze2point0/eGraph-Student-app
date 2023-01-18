49a46b87f726b59d12309abf15bd8a5e5c960d88
Antonin Carpentier <carpentier.antonin.1@gmail.com>
2023-01-18 18:17:00 +0100

    JMenu

--- gui/Gui.java
+++ gui/Gui.java
@@ -1,6 +1,6 @@
 package gui;
-import java.awt.event.ActionEvent;
-import java.awt.event.KeyEvent;
+import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
 
 public class Gui extends JFrame{
@@ -24,37 +24,88 @@ public class Gui extends JFrame{
         //File section
         JMenu menuFile = new JMenu("File");
             menuFile.setMnemonic('F');
-        JMenuItem itemFile = new JMenuItem("New graph");
-            //mnemonic
+        JMenuItem itemFile = new JMenuItem("New...");
+            itemFile.setMnemonic('N');
             //icon
-            //accelerator
+            itemFile.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, KeyEvent.CTRL_DOWN_MASK));
             //actionlistener
             menuFile.add(itemFile);
-        itemFile = new JMenuItem("New automaton");
-            //mnemonic
+
+        itemFile = new JMenuItem("Open");
+            itemFile.setMnemonic('O');
             //icon
-            //accelerator
+            itemFile.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O, KeyEvent.CTRL_DOWN_MASK));
             //actionlistener
             menuFile.addSeparator();
             menuFile.add(itemFile);
-            
+
         itemFile = new JMenuItem("Save");
             itemFile.setMnemonic('S');
+            //icon
             itemFile.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, KeyEvent.CTRL_DOWN_MASK));
             //actionlistener
             menuFile.addSeparator();
             menuFile.add(itemFile);
 
+        itemFile = new JMenuItem("Export");
+            itemFile.setMnemonic('E');
+            //icon
+            itemFile.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_E, KeyEvent.CTRL_DOWN_MASK));
+            //actionlistener
+            menuFile.addSeparator();
+            menuFile.add(itemFile);
+        
+        itemFile = new JMenuItem("Exit");
+            //icon
+            itemFile.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0));
+            itemFile.addActionListener(new ActionListener(){
+                public void actionPerformed(ActionEvent e){
+                    System.exit(0);
+                }
+            });
+            menuFile.addSeparator();
+            menuFile.add(itemFile);
+            
         menuBar.add(menuFile);
-
-
-
-
 
 
         //Edit section
         JMenu menuEdit = new JMenu("Edit");
         menuFile.setMnemonic('E');
+
+        JMenuItem itemEdit = new JMenuItem("Undo");
+            //icon
+            itemEdit.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Z, KeyEvent.CTRL_DOWN_MASK));
+            //actionlistener
+            menuEdit.add(itemEdit);
+
+        itemEdit = new JMenuItem("Redo");
+            //icon
+            itemEdit.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Y, KeyEvent.CTRL_DOWN_MASK));
+            //actionlistener
+            menuEdit.addSeparator();
+            menuEdit.add(itemEdit);
+        
+        itemEdit = new JMenuItem("Cut");
+            //icon
+            itemEdit.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_X, KeyEvent.CTRL_DOWN_MASK));
+            //actionlistener
+            menuEdit.addSeparator();
+            menuEdit.add(itemEdit);
+    
+        itemEdit = new JMenuItem("Copy");
+            //icon
+            itemEdit.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_C, KeyEvent.CTRL_DOWN_MASK));
+            //actionlistener
+            menuEdit.addSeparator();
+            menuEdit.add(itemEdit);
+
+        itemEdit = new JMenuItem("Paste");
+            //icon
+            itemEdit.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_V, KeyEvent.CTRL_DOWN_MASK));
+            //actionlistener
+            menuEdit.addSeparator();
+            menuEdit.add(itemEdit);
 
         menuBar.add(menuEdit);
 
@@ -67,27 +118,58 @@ public class Gui extends JFrame{
         JMenu menuAlgorithm = new JMenu("Algorithm");
         menuFile.setMnemonic('A');
 
+        JMenuItem itemAlgorithm = new JMenuItem("Launch Algorithm...");
+            //icon
+            itemAlgorithm.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_A, KeyEvent.CTRL_DOWN_MASK));
+            //actionlistener
+            menuAlgorithm.add(itemAlgorithm);
+
+            itemAlgorithm = new JMenuItem("Algorithm informations");
+            //icon
+            itemAlgorithm.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_I, KeyEvent.CTRL_DOWN_MASK));
+            //actionlistener
+            menuAlgorithm.addSeparator();
+            menuAlgorithm.add(itemAlgorithm);
+
+
         menuBar.add(menuAlgorithm);
 
 
+        // //Settings section
+        // JMenu menuSettings = new JMenu("Settings");
+        // menuFile.setMnemonic('S');
 
 
-
-
-        //Settings section
-        JMenu menuSettings = new JMenu("Settings");
-        menuFile.setMnemonic('S');
-
-        menuBar.add(menuSettings);
-
-
-
-
+        // menuBar.add(menuSettings);
 
 
         //Help section
         JMenu menuHelp = new JMenu("Help");
-        menuFile.setMnemonic('H');
+        menuHelp.setMnemonic('H');
+
+        JMenuItem itemHelp = new JMenuItem("Shortcuts...");
+            //icon
+            //actionlistener
+            menuHelp.add(itemHelp);
+  
+            itemHelp = new JMenuItem("Documentation...");
+            //icon
+            //actionlistener
+            menuHelp.addSeparator();
+            menuHelp.add(itemHelp);
+
+            itemHelp = new JMenuItem("Licence...");
+            //icon
+            //actionlistener
+            menuHelp.addSeparator();
+            menuHelp.add(itemHelp);
+
+            itemHelp = new JMenuItem("About...");
+            //icon
+            //actionlistener
+            menuHelp.addSeparator();
+            menuHelp.add(itemHelp);
+        
 
         menuBar.add(menuHelp);
 
