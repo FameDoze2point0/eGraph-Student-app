d839a321f4fa79bcdda9953e50a12cb2b93faafa
Antonin <carpentier.antonin.1@gmail.com>
2023-02-14 18:45:01 +0100

    backend
    algorithmes principaux implémentés

--- Main.java
+++ Main.java
@@ -37,37 +37,55 @@ public class Main
         g.addVertex(vF);
         g.addVertex(vG);
 
-        vA.addEdge( vB, 2, 1, Color.black);
-        vA.addEdge( vC, 1, 1, Color.black);
+        g.addEdge(vA,vB, 2, 1, Color.black);
+        g.addEdge(vA,vC, 1, 1, Color.black);
 
-        vB.addEdge( vC, 1, 1, Color.black);
+        g.addEdge(vB,vC, 3, 1, Color.black);
 
-        vC.addEdge( vD, 1, 1, Color.black);
-        vC.addEdge( vE, 1, 1, Color.black);
-        vC.addEdge( vF, 1, 1, Color.black);
+        g.addEdge(vC,vD, 3, 1, Color.black);
+        g.addEdge(vC,vE ,2, 1, Color.black);
+        g.addEdge(vC,vF, 1, 1, Color.black);
 
-        vD.addEdge( vA, 1, 1, Color.black);
+        g.addEdge(vD,vA, 3, 1, Color.black);
 
-        vE.addEdge( vG, 1, 1, Color.black);
+        g.addEdge(vE,vG, 3, 1, Color.black);
 
-        vG.addEdge( vB, 1, 1, Color.black);
+        g.addEdge(vF,vD, 3, 1, Color.black);
+
+        g.addEdge(vG,vB, 2, 1, Color.black);
 
         System.out.println(g.toString());
 
         System.out.println("\nBFS :");
-        g.algo_BFS(vC);
+        g.algo_BFS(vA);
 
         System.out.println("\nDFS :");
-        g.algo_DFS(vC);
+        g.algo_DFS(vA);
 
         System.out.println("\nRS :");
-        g.algo_RS(vC);
+        g.algo_RS(vA);
 
-        System.out.println("\nDijkstra :");
-        g.algo_Dijkstra(vA);
+        try {
+            System.out.println("\nDijkstra :");
+            g.algo_Dijkstra(vA);
 
-        System.out.println("\nBellman-Ford");
-        g.algo_BellmanFord(vA);
+            System.out.println("\nBellman-Ford :");
+            g.algo_BellmanFord(vA);
+
+            System.out.println("\nFloyd-Warshall :");
+            g.algo_FloydWarshall();
+
+            // System.out.println("\nPrim :");
+            // g.algo_Prim(vA);
+
+            // System.out.println("\nKruskal :");
+            // g.algo_Kruskal();
+
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        
 
     }
 }
--- util/Triplet.java
+++ util/Triplet.java
@@ -1,5 +1,5 @@
 package util;
-public class Triplet {
+public class Triplet implements Comparable<Triplet>{
     Vertex vertex;
     Edge edge;
     int weight;
@@ -8,5 +8,13 @@ public class Triplet {
         this.vertex = vertex;
         this.edge = edge;
         this.weight = weight;
+    }
+
+    public int compareTo(Triplet compareTo){
+        if (weight == compareTo.weight)
+            return 0;
+        if (weight > compareTo.weight)
+            return 1;
+        return -1;
     }
 }
